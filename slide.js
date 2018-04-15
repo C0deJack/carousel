1d31fab10007661332fa2774738e418f675b8c70
C0deJack <jackwoodpearce@gmail.com>
2018-04-15 13:36:25 +0100

    created three functions that change the visiblity of the pictures on click of the arrows and also on set time out

--- slide.js
+++ slide.js
@@ -1,14 +1,39 @@
 var rightButton = document.getElementById('buttonRight')
-
-rightButton.addEventListener('click', slide);
-
-function slide() {
-    console.log(changeCurrentPicture())
-}
+var leftButton = document.getElementById('buttonLeft')
+var picOne = document.getElementById('one')
+var picTwo = document.getElementById('two')
+var picThree = document.getElementById('three')
+var picFour = document.getElementById('four')
 
 var currentPicture = 1;
 
-function changeCurrentPicture() {
+setInterval(function(){ changeCurrentPictureUp(); }, 5000);
+
+rightButton.addEventListener('click', changeCurrentPictureUp);
+leftButton.addEventListener('click', changeCurrentPictureDown);
+
+function displayCurrentPicture() {
+    picOne.classList.remove("visible");
+    picTwo.classList.remove("visible");
+    picThree.classList.remove("visible");
+    picFour.classList.remove("visible");
+    switch (currentPicture) {
+        case 1:
+            picOne.classList.add("visible");
+            break;
+        case 2:
+            picTwo.classList.add("visible");
+            break;
+        case 3:
+            picThree.classList.add("visible");
+            break;
+        case 4:
+            picFour.classList.add("visible");
+            break;
+    }
+}
+
+function changeCurrentPictureUp() {
     switch (currentPicture) {
         case 1:
             currentPicture = 2;
@@ -23,5 +48,24 @@ function changeCurrentPicture() {
             currentPicture = 1;
             break;
     }
-    return currentPicture
+    displayCurrentPicture()
 }
+
+function changeCurrentPictureDown() {
+    switch (currentPicture) {
+        case 1:
+            currentPicture = 4;
+            break;
+        case 2:
+            currentPicture = 1;
+            break;
+        case 3:
+            currentPicture = 2;
+            break;
+        case 4:
+            currentPicture = 3;
+            break;
+    }
+    displayCurrentPicture()
+}
+
