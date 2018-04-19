6b4bd1b588417da0db8fd7b76b831f476a3fb85e
C0deJack <jackwoodpearce@gmail.com>
2018-04-19 08:54:46 +0100

    changed the z-index to fix issue with picture four coming to the top when not needed. Added text tot he bottom of the picture and amimated the class to suit

--- slide.js
+++ slide.js
@@ -1,34 +1,70 @@
-var rightButton = document.getElementById('buttonRight')
-var leftButton = document.getElementById('buttonLeft')
-var picOne = document.getElementById('one')
-var picTwo = document.getElementById('two')
-var picThree = document.getElementById('three')
-var picFour = document.getElementById('four')
+var rightButton = document.getElementById('buttonRight');
+var leftButton = document.getElementById('buttonLeft');
+var picOne = document.getElementById('img1');
+var picTwo = document.getElementById('img2');
+var picThree = document.getElementById('img3');
+var picFour = document.getElementById('img4');
+var textOne = document.getElementById('text1');
+var textTwo = document.getElementById('text2');
+var textThree = document.getElementById('text3');
+var textFour = document.getElementById('text4');
 
-var currentPicture = 1;
+var currentPicture = 4;
 
-setInterval(function(){ changeCurrentPictureUp(); }, 5000);
+//var sliding = null;
+
+//window.onload = startSlide;
+
+// function startSlide() {
+//     if (!sliding) {
+//         sliding = setInterval(changeCurrentPictureUp, 3000);
+//     }
+// }
+//
+// function pauseSlide() {
+//     if (sliding) {
+//         clearInterval(sliding);
+//         sliding = null;
+//         setTimeout(changeCurrentPictureUp, 6000 );
+//     }
+// }
+
+//setInterval(function(){ changeCurrentPictureUp(); }, 5000);
 
 rightButton.addEventListener('click', changeCurrentPictureUp);
 leftButton.addEventListener('click', changeCurrentPictureDown);
+//rightButton.addEventListener('click',  pauseSlide);
+//leftButton.addEventListener('click', pauseSlide);
 
 function displayCurrentPicture() {
-    picOne.classList.remove("visible");
-    picTwo.classList.remove("visible");
-    picThree.classList.remove("visible");
-    picFour.classList.remove("visible");
+    picOne.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
+    picTwo.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
+    picThree.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
+    picFour.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
+    textOne.classList.remove("textVisible");
+    textTwo.classList.remove("textVisible");
+    textThree.classList.remove("textVisible");
+    textFour.classList.remove("textVisible");
     switch (currentPicture) {
         case 1:
-            picOne.classList.add("visible");
+            picOne.classList.add("slideOutRight");
+            picTwo.classList.add("slideInRight");
+            textTwo.classList.add("textVisible");
             break;
         case 2:
-            picTwo.classList.add("visible");
+            picTwo.classList.add("slideOutRight");
+            picThree.classList.add("slideInRight");
+            textThree.classList.add("textVisible");
             break;
         case 3:
-            picThree.classList.add("visible");
+            picThree.classList.add("slideOutRight");
+            picFour.classList.add("slideInRight");
+            textFour.classList.add("textVisible");
             break;
         case 4:
-            picFour.classList.add("visible");
+            picFour.classList.add("slideOutRight");
+            picOne.classList.add("slideInRight");
+            textOne.classList.add("textVisible");
             break;
     }
 }
