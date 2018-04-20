aa62427889c8534416aedefe3ebdddaad0e15e07
C0deJack <jackwoodpearce@gmail.com>
2018-04-20 09:54:20 +0100

    Removed two of the switches to reduce code and commented out the set interval and this is working in both directions

--- slide.js
+++ slide.js
@@ -9,34 +9,33 @@ var textTwo = document.getElementById('text2');
 var textThree = document.getElementById('text3');
 var textFour = document.getElementById('text4');
 
-var currentPicture = 4;
+var currentPicture = 1;
 
-//var sliding = null;
-
-//window.onload = startSlide;
-
-// function startSlide() {
-//     if (!sliding) {
-//         sliding = setInterval(changeCurrentPictureUp, 3000);
-//     }
-// }
+// var foo = setInterval(function() {
+//     changeCurrentPictureUp();
+//     }, 5000);
 //
-// function pauseSlide() {
-//     if (sliding) {
-//         clearInterval(sliding);
-//         sliding = null;
-//         setTimeout(changeCurrentPictureUp, 6000 );
-//     }
-// }
+// var boo = setInterval(function() {
+//     changeCurrentPictureDown();
+// }, 5000);
 
-//setInterval(function(){ changeCurrentPictureUp(); }, 5000);
+rightButton.addEventListener('click', function() {
+    changeCurrentPictureUp()
+    clearInterval(foo)
+    foo = setInterval(function() {
+        changeCurrentPictureUp();
+        }, 5000);
+});
 
-rightButton.addEventListener('click', changeCurrentPictureUp);
-leftButton.addEventListener('click', changeCurrentPictureDown);
-//rightButton.addEventListener('click',  pauseSlide);
-//leftButton.addEventListener('click', pauseSlide);
+leftButton.addEventListener('click', function() {
+    changeCurrentPictureDown()
+    clearInterval(boo)
+   boo = setInterval(function() {
+        changeCurrentPictureDown();
+    }, 5000);
+});
 
-function displayCurrentPicture() {
+function changeCurrentPictureUp() {
     picOne.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
     picTwo.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
     picThree.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
@@ -50,58 +49,64 @@ function displayCurrentPicture() {
             picOne.classList.add("slideOutRight");
             picTwo.classList.add("slideInRight");
             textTwo.classList.add("textVisible");
+            currentPicture = 2;
             break;
         case 2:
             picTwo.classList.add("slideOutRight");
             picThree.classList.add("slideInRight");
             textThree.classList.add("textVisible");
+            currentPicture = 3;
             break;
         case 3:
             picThree.classList.add("slideOutRight");
             picFour.classList.add("slideInRight");
             textFour.classList.add("textVisible");
+            currentPicture = 4;
             break;
         case 4:
             picFour.classList.add("slideOutRight");
             picOne.classList.add("slideInRight");
             textOne.classList.add("textVisible");
-            break;
-    }
-}
-
-function changeCurrentPictureUp() {
-    switch (currentPicture) {
-        case 1:
-            currentPicture = 2;
-            break;
-        case 2:
-            currentPicture = 3;
-            break;
-        case 3:
-            currentPicture = 4;
-            break;
-        case 4:
             currentPicture = 1;
             break;
     }
-    displayCurrentPicture()
 }
 
 function changeCurrentPictureDown() {
+    picOne.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
+    picTwo.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
+    picThree.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
+    picFour.classList.remove("slideInLeft", "slideOutLeft", "slideInRight", "slideOutRight");
+    textOne.classList.remove("textVisible");
+    textTwo.classList.remove("textVisible");
+    textThree.classList.remove("textVisible");
+    textFour.classList.remove("textVisible");
     switch (currentPicture) {
         case 1:
+            picOne.classList.add("slideOutLeft");
+            picFour.classList.add("slideInLeft");
+            textFour.classList.add("textVisible");
             currentPicture = 4;
             break;
-        case 2:
-            currentPicture = 1;
-            break;
-        case 3:
-            currentPicture = 2;
-            break;
         case 4:
+            picFour.classList.add("slideOutLeft");
+            picThree.classList.add("slideInLeft");
+            textThree.classList.add("textVisible");
             currentPicture = 3;
             break;
+        case 3:
+            picThree.classList.add("slideOutLeft");
+            picTwo.classList.add("slideInLeft");
+            textTwo.classList.add("textVisible");
+            currentPicture = 2;
+            break;
+        case 2:
+            picTwo.classList.add("slideOutLeft");
+            picOne.classList.add("slideInLeft");
+            textOne.classList.add("textVisible");
+            currentPicture = 1;
+            break;
     }
-    displayCurrentPicture()
 }
+
 
